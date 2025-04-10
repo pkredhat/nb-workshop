879da00ad4fc2af72c7e391cb0583f061d5eb931
Phillip Knezevich <phknezev@redhat.com>
2025-04-10 21:33:29 +0000

    2nd ui added

--- routes/main.py
+++ routes/main.py
@@ -6,30 +6,21 @@ import requests
 
 main = Blueprint('main', __name__)
 
-
-
 @main.route("/", methods=["GET"])
 def index():
-    return Response(f"Boiler plate home.", mimetype='text/plain')
-
-
-@main.route("/api/force500")
-def force_500():
-    abort(500, description="Intentional 500 error for demonstration purposes")
-
-@main.route("/api/force200")
-def force_200():
-    return "This is a forced 200 OK response.", 200
+    return Response(f"200 OK", mimetype='text/plain')
 
 @main.route('/api/check', methods=['POST'])
 def check_json():
     # Try to parse the JSON body
-    data = request.get_json()
-
-    # Check if JSON was provided and if "musthave" is in the JSON object.
-    if not data or 'musthave' not in data:
-        # Aborting with a 500 error if "musthave" is missing
-        abort(500, description="'musthave' key missing in JSON body")
-
-    # Return a 200 OK response with a simple JSON message if key is present.
-    return jsonify({"message": "'musthave' key received", "data": data}), 200
+    data = request.get_json()    
+    if not data or 'amt' not in data:
+        abort(500, description="'amt' key missing in JSON body")
+    try:
+        amt = float(data.get('amount'))
+    except (ValueError, TypeError):
+        abort(500, description="Invalid")    
+    if amt < 25000:
+        abort(500, description="Invalid")
+    else:
+        return jsonify({"message": "Valid", "data": data}), 200
