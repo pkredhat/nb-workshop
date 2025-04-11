5aa1e027d3810cad960a8d0eee9ec0c2ef87a235
pkredhat <120052595+pkredhat@users.noreply.github.com>
2025-04-11 00:27:47 -0400

    added hjttp client

--- src/Program.cs
+++ src/Program.cs
@@ -6,6 +6,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddHttpClient();
         builder.Services.AddOpenApi();            
         // Add services to the container.
         builder.Services.AddControllers();
