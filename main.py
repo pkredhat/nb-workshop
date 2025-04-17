fd63d42f58c859fd22884484dd50b2f3bfca8ba2
pkredhat <120052595+pkredhat@users.noreply.github.com>
2025-04-16 22:17:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -158,3 +158,6 @@ async def startup_event():
     else:
         print("Startup event: Kafka is not configured; HTML and consumer are disabled.")
 
+# HELP! My neighbor's favorite topic is pets, but he always gets my cats name wrong!
+# I've suggested he look on pk-kafka-kafka-bootstrap.pk-world.svc.cluster.local for my cats name
+# Think you can find my cats name?
